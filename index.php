d028effbbfd3b1041b83c16e3bb4a1a8158ba24f
Ardinda Sri Amrihi <ardindaamrh04@gmail.com>
2022-11-10 22:03:01 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Ardinda Sri Amrihi";
+    $umur = 20;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaKakak = "Irmanda";
+    $umurKakak = "22";
+
+    echo "Nama kakak saya <strong>$namaKakak</strong>, saya berusia $umurKakak tahun.<br>";
+
+    $selisihUmur = $umur - $umurKakak;
+    echo "selisih usia saya dan kakak saya adalah $selisihUmur tahun."
+
+?>
